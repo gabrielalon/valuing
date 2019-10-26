757c2dc1f163cf4f097b2b1f7a0b383429b19aeb
Marek Rode <m.rode@i-systems.pl>
2019-10-26 14:38:22 +0200

    fix uuid

--- src/Identity/Uuid.php
+++ src/Identity/Uuid.php
@@ -5,7 +5,7 @@ namespace N3ttech\Valuing\Identity;
 use Assert\Assertion;
 use N3ttech\Valuing\VO;
 
-final class Uuid extends VO
+final class Uuid extends VO implements AggregateId
 {
     /**
      * @param string $uuid
